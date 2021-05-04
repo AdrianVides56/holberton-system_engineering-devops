6ef5ca58ccde8f791e91147d745951f7e304b4c8
AdrianVides56 <adrianvj18@gmail.com>
2021-05-03 20:37:02 -0500

    Added task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Using what done on task #0, extend your
+the script to export data in the JSON format.
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    """Prevent execution of this code when imported"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = argv[1]
+    user_dict = {user_id: []}
+
+    user_name = requests.get(url + 'users/{}'.format(user_id)).json()
+    user_tasks = requests.get(url + 'users/{}/todos'.format(user_id)).json()
+
+    for task in user_tasks:
+        user_dict[user_id].append({'task': task['title'],
+                                   'completed': task['completed'],
+                                   'username': user_name['username']})
+
+    with open('{}.json'.format(user_id), 'w') as json_f:
+        json_f.write(json.dumps(user_dict))
