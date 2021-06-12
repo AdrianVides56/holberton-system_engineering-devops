2cd402692c0b4173a20a01eda19cdedd8a80c506
AdrianVides56 <adrianvj18@gmail.com>
2021-06-12 17:50:38 -0500

    Added task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Working with Reddit API
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    agent = {'User-Agent': 'termi'}
+    try:
+        req = requests.get(url, headers=agent, allow_redirects=False).json()
+    except:
+        return 0
+    a = req.get('data').get('subscribers')
+    return a
